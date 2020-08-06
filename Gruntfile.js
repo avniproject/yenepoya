d4d74031ab6ff0f35a736e55eeaabadf08654294
sachin kadam <sachinkadamsk01@gmail.com>
2020-08-06 15:53:45 +0530

    changes ++ 06 aug 2020

--- Gruntfile.js
+++ Gruntfile.js
@@ -27,10 +27,26 @@ module.exports = IDI.configure(
                 "forms/NCD Enrolment.json",
                 "forms/NCD Followup.json",
                 "forms/Program encounter cancellation.json",
-                "forms/Program exit form.json"
-
-
-
+                "forms/Program exit form.json",
+                "forms/ANC Followup.json",
+                "forms/ANC Followup Encounter Cancellation (voided~979).json",
+                "forms/Batch Registration (voided~237).json",
+                "forms/Child Enrolment.json",
+                "forms/Child Growth and Health status Followup.json",
+                "forms/Diabetes Exit (voided~720).json",
+                "forms/ET Enrolment (voided~232).json",
+                "forms/ET Exit (voided~233).json",
+                "forms/ET Session Followup Encounter (voided~234).json",
+                "forms/ET Session Followup Encounter Cancellation (voided~235).json",
+                "forms/Household Registration(voided).json",
+                "forms/Hypertension Exit (voided~725).json",
+                "forms/Mother PNC Encounter Cancellation (voided~1008).json",
+                "forms/NCD  Exit (voided~745).json",
+                "forms/PNC Followup.json",
+                "forms/Pregnancy Enrolment.json",
+                "forms/Pregnancy Exit (voided~975).json",
+                "forms/Pregnancy Outcome.json",
+                "forms/Vaccination Checklist Form.json"
             ],
             formMappings: ["formMappings.json"],
             formDeletions: [],
@@ -38,7 +54,7 @@ module.exports = IDI.configure(
             catchments: [
                "metadata/sample_catchment.json"
             ],
-            checklistDetails: ["checklist.json"],
+           // checklistDetails: ["checklist.json"],
             concepts: ["concepts.json"],
             addressLevelTypes: [],
             locations: ["metadata/location.json"],
